f7760a12153bc4d703cd531cb41356198d3e0bee
Andrew T. Baker <andrew.tork.baker@gmail.com>
2014-12-12 17:41:30 -0500

    Quitting for today

--- wikitrivia/article.py
+++ wikitrivia/article.py
@@ -5,7 +5,6 @@ class Article:
     """Retrieves and analyzes wikipedia articles"""
 
     def __init__(self, title):
-        self.title = title
         self.page = wikipedia.page(title)
         self.summary = TextBlob(self.page.summary)
         self.body = TextBlob(self.page.content)
@@ -14,8 +13,13 @@ class Article:
         return self.body.words.count(word) < 5
 
     def generate_trivia_sentences(self):
+        sentences = self.summary.sentences
+
+        # Remove the first sentence - it's never a good one
+        del sentences[0]
+
         trivia_sentences = []
-        for sentence in self.summary.sentences:
+        for sentence in sentences:
             trivia = self.evaluate_sentence(sentence)
             if trivia:
                 trivia_sentences.append(str(trivia))
@@ -23,23 +27,27 @@ class Article:
         return trivia_sentences
 
     def evaluate_sentence(self, sentence):
+        tag_map = {word.lower(): tag for word, tag in sentence.tags}
+
         replace_nouns = []
-        for counter, tag in enumerate(sentence.tags):
-            if tag[1] == 'NN':
-                noun = tag[0]
-                
+        for word, tag in sentence.tags:
+            if tag == 'NN':
                 # Is it unusual compared to other words in this article? 
                 # If not, it probably won't make for good trivia
-                if not self.is_unusual_word(noun):
+                if not self.is_unusual_word(word):
                     break
                 
                 # Is it in a noun phrase? If so, blank out everything in that phrase
                 for phrase in sentence.noun_phrases:
-                    if noun in phrase:
+                    if phrase[0] == '\'':
+                        # If it starts with an apostrophe, ignore it
+                        break
+
+                    if word in phrase:
                         [replace_nouns.append(word) for word in phrase.split()]
                         break
                     else:
-                        replace_nouns.append(noun)
+                        replace_nouns.append(word)
                 break
         
         if len(replace_nouns) == 0:
