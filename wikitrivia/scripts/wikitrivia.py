7e54d2700bca8148d4d791e3ab45c328a2552fbc
Andrew T. Baker <andrew.tork.baker@gmail.com>
2014-12-17 00:16:21 -0500

    Temporarily hard-coding a few articles

--- wikitrivia/scripts/wikitrivia.py
+++ wikitrivia/scripts/wikitrivia.py
@@ -3,13 +3,31 @@ from wikitrivia.article import Article
 import click
 import json
 
+# Tony Bennett
+# The Room
+# Denmark
+# Benedict Arnold
+# Communism
+# Ukranian Women's Volleyball Super-league
+
+ARTICLES = (
+    'Tony Bennett',
+    'The Room (film)',
+    'Denmark',
+    'Benedict Arnold',
+    'Communism',
+    'Ukrainian Women\'s Volleyball Super League'
+)
+
 @click.command()
 @click.argument('title')
 @click.option('--output', default=False, help='Output to JSON file')
 def generate_trivia(title, output):
     """Generates trivia questions from wikipedia articles"""
-    article = Article(title=title)
-    questions = article.generate_trivia_sentences()
+    questions = []
+    for article in ARTICLES:
+        article = Article(title=article)
+        questions = questions + article.generate_trivia_sentences()
 
     if output:
       with open(output, 'w') as json_file:
